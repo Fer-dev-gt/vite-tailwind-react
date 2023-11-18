4306469b5f7af075e911fc2727fc8f4223bff6a8
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-11-17 21:56:18 -0600

    Implemented global Layout Component for unified styles

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,4 +1,3 @@
-
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
@@ -6,7 +5,7 @@ const Navbar = () => {
   const isRouteActive = (isActive) => isActive ? activeStyle : undefined
 
   return (
-    <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
+    <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
       <ul className='flex items-center gap-3'>
         <li className='font-semibold text-lg'>
           <NavLink to='/'>
