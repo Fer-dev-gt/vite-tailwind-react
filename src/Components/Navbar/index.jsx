ed506fb5d4c1012cdf681706b62864c47652d0f6
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-11-17 21:34:58 -0600

    Added Navbar Component with 'NavLink' integrated and added tailwind styles

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,119 @@
+
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const activeStyle = 'underline underline-offset-4';
+  const isRouteActive = (isActive) => isActive ? activeStyle : undefined
+
+  return (
+    <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
+      <ul className='flex items-center gap-3'>
+        <li className='font-semibold text-lg'>
+          <NavLink to='/'>
+            Shopi
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to='/'
+            className={({ isActive }) => 
+              isRouteActive(isActive)
+            }
+          > 
+            All
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to='/clothes'
+            className={({ isActive }) => 
+              isRouteActive(isActive)
+            }
+          >
+            Clothes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to='/electronics'
+            className={({ isActive }) => 
+              isRouteActive(isActive)
+            }
+          >
+            Electronics
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to='/furnitures'
+            className={({ isActive }) => 
+              isRouteActive(isActive)
+            }
+          >
+            Furnitures
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to='/toys'
+            className={({ isActive }) => 
+              isRouteActive(isActive)
+            }
+          >
+            Toys
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to='/others'
+            className={({ isActive }) => 
+              isActive ? activeStyle : undefined
+            }
+          >
+            Others
+          </NavLink>
+        </li>
+      </ul>
+      <ul className='flex items-center gap-3'>
+        <li className='text-black/60'>
+          fer-dev@gmail.com
+        </li>
+        <li>
+          <NavLink 
+            to='/my-orders'
+            className={({ isActive }) => 
+              isRouteActive(isActive)
+            }
+          >
+            My Orders
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to='/my-account'
+            className={({ isActive }) => 
+              isRouteActive(isActive)
+            }
+          >
+            My Account
+          </NavLink>
+        </li>
+        <li>
+          <NavLink 
+            to='/sign-in'
+            className={({ isActive }) => 
+              isRouteActive(isActive)
+            }
+          >
+            Sign In
+          </NavLink>
+        </li>
+        <li>
+          🛒 0
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -5,6 +5,7 @@ import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import SignIn from '../SignIn'
 import NotFound from '../NotFound'
+import NavBar from '../../Components/Navbar'
 import './App.css'
 
 
@@ -26,6 +27,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <AppRoutes />
+      <NavBar />
     </BrowserRouter>
   )
 }
