6b78b26b4eb0bab7afb473fec5976d07b84f7635
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-11-18 19:10:05 -0600

    Consumed Platzi Store API to generate a catalog of card products

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const isRouteActive = (isActive) => isActive ? activeStyle : undefined
 
   return (
-    <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
+    <nav className='bg-white flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
       <ul className='flex items-center gap-3'>
         <li className='font-semibold text-lg'>
           <NavLink to='/'>
