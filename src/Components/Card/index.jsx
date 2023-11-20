41bc3bf8664f359f66ec5f59c57d34a491d94ead
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-11-19 19:35:30 -0600

    Use context to set an open/close productDetail Component buttons

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -3,19 +3,30 @@ import { PlusIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context';
 
 const Card = (data) => {
-  const { count, setCount } = useContext(ShoppingCartContext)
+  const { 
+    count, 
+    setCount, 
+    openProductDetail 
+  } = useContext(ShoppingCartContext);
+
   useEffect(() => {
     console.log('COUNT: ', count);
   }, [count]);
 
   return (
-    <div className='bg-white cursor-pointer w-56 h-64 rounded-lg shadow-md'>
+    <div 
+    className='bg-white cursor-pointer w-56 h-64 rounded-lg shadow-md'
+    onClick={() => openProductDetail()}>
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xm m-2 px-3 py-0.5'>{data.data.category.name}</span>
         <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.title}/>
         <div 
           className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 hover:bg-blue-500'
-          onClick={() => setCount(count + 1)}>
+          onClick={(e) => {
+            e.stopPropagation();
+            setCount(count + 1);
+          }
+        }>
           <PlusIcon className='h-4 w-4'/>
         </div>
       </figure>
--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,13 +1,21 @@
+import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ShoppingCartContext } from '../../Context'
 import './styles.css'
 
 const ProductDetail = () => {
+
+  const { isProductDetailOpen, closeProductDetail } = useContext(ShoppingCartContext)
+
   return (
-    <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white'>
+    <aside 
+      className={`${isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>Detail</h2>
         <div>
-          <XMarkIcon className='h-6 w-6'/>
+          <XMarkIcon 
+            className='h-6 w-6'
+            onClick={closeProductDetail}/>
         </div>
       </div>
     </aside>
