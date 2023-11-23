8ecfc43e9f5f2be9b452914a8ed86d2cc69ea109
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-11-22 21:26:59 -0600

    Added styles to list of orders

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,30 +1,33 @@
-import { ChevronRightIcon, CalendarDaysIcon, CurrencyDollarIcon, ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { ChevronRightIcon, CalendarDaysIcon, ShoppingCartIcon } from '@heroicons/react/24/outline';
 
 const OrdersCard = props => {
   const { totalPrice, totalProducts, date} = props;
 
   return (
-      <div className="flex justify-between items-center mb-3 border rounded-lg p-3">
-        <div className="flex items-center justify-between grow gap-2 px-4">
-          <div className="flex gap-1 items-center justify-center">
-            <ShoppingCartIcon className="h-6 w-6 text-black" />
-            <p className="font-light text-sm">{`${totalProducts} ${totalProducts === 1 ? "producto" : "productos"}`}</p>
-          </div>
-          <div className="flex gap-1 items-center justify-center">
-            <CurrencyDollarIcon className="h-6 w-6 text-black" />
-            <p className="font-light text-sm">${totalPrice}</p>
-          </div>
-          <div className="flex gap-1 items-center justify-center">
-            <CalendarDaysIcon className="h-6 w-6 text-black" />
-            <p className="font-light text-sm">{date}</p>
-          </div>
+    <div className='flex justify-between items-center mb-3 border border-black rounded-lg p-4 w-96'>
+      <div className='flex justify-between w-full'>
+        <div className='flex flex-col gap-1'>
+          <span className='font-light flex'>
+            <CalendarDaysIcon className='h-6 w-6 text-black' />
+            <div className='ml-2'>{date}</div>
+          </span>
+          <span className='font-light flex'>
+            <ShoppingCartIcon className='h-6 w-6 text-black' />
+            <div className='ml-2'>{`${totalProducts} ${totalProducts === 1 ? 'product' : 'products'}`}</div>
+          </span>
         </div>
-        <div className="flex items-center gap-2">
-          <ChevronRightIcon className="h-6 w-6 text-black" />
+        <div className='flex items-center gap-1'>
+          <span className='font-medium text-2xl flex'>
+            <div>${totalPrice}</div>
+          </span>
+          <ChevronRightIcon className='h-6 w-6 cursor-pointer'/>
         </div>
       </div>
+    </div>
   )
 }
+
+
 
 export default OrdersCard
  
