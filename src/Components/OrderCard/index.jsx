31fca3c4b5484626c2a8917f13ad87664eb3c64f
Fernando Orozco <jose.acciones.gt@gmail.com>
2023-12-06 18:58:41 -0600

    Simplified Xicon for removing product from shopping cart

--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -2,12 +2,6 @@ import { XMarkIcon } from '@heroicons/react/24/solid'
 
 const OrderCard = props => {
   const { id, title, imageUrl, price, quantity, handleDelete } = props;
-  let renderXMarkIcon;
-
-  if (handleDelete) {
-    renderXMarkIcon = 
-    <XMarkIcon className='h-6 w-6 cursor-pointer' onClick={() => handleDelete(id, quantity)}/>
-  }
 
   return (
     <div className='flex justify-between items-center mb-3'>
@@ -20,10 +14,9 @@ const OrderCard = props => {
       </div>
       <div className='flex items-center gap-2'>
         <p className='text-lg font-medium'>${price * quantity}</p>
-        { renderXMarkIcon }
+        <XMarkIcon className='h-6 w-6 cursor-pointer' onClick={() => handleDelete(id, quantity)}/>
       </div> 
     </div>
-    
   )
 }
 
